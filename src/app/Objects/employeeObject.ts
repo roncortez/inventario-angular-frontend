3cbb592517a9872f57cf8f42dd135b119805895a
roncortez <108027540+roncortez@users.noreply.github.com>
2022-08-02 23:44:28 -0500

    Phase 1

---
+++ src/app/Objects/employeeObject.ts
@@ -0,0 +1,9 @@
+export class employeeObjectVo {
+    firstname: string;
+    lastname: string;
+    email: string;
+    documentId: string; 
+    password: string;
+    isAdmin: boolean;
+    username: string;
+}
--- src/app/component/registerform/registerform.component.ts
+++ src/app/component/registerform/registerform.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { SwitchService } from 'src/app/services/switch.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { employeeObjectVo } from 'src/app/Objects/employeeObject';
 
 @Component({
   selector: 'app-registerform',
@@ -12,7 +13,16 @@ export class RegisterformComponent implements OnInit {
   modalSwitch:boolean;
   registerForm: FormGroup | any;
 
+  //angular component
+  @Output() addEmployeeEmiter = new EventEmitter();
+  
+
   constructor(private modalSS: SwitchService) { 
+    //call services
+  }
+
+  ngOnInit(): void {
+    
     this.registerForm = new FormGroup({
       id: new FormControl('', [Validators.required,Validators.pattern(
         '[0-9]{10}')]),
@@ -22,10 +32,8 @@ export class RegisterformComponent implements OnInit {
         '^[a-zA-Z ]*$')]),
       email: new FormControl('', [Validators.required, Validators.email]),
     });
-  }
-
-  ngOnInit(): void {
     this.modalSS.$modal.subscribe((valor)=>(this.modalSwitch = valor))
+    
   }
   
   openModal(){
@@ -36,5 +44,23 @@ export class RegisterformComponent implements OnInit {
     this.modalSS.$modal.emit(false);
   }
 
+  addEmployee(){
+    
+    let newEmployee:employeeObjectVo = new employeeObjectVo()
+
+    newEmployee.documentId = this.registerForm.controls.id.value
+    newEmployee.firstname = this.registerForm.controls.firstname.value
+    newEmployee.lastname = this.registerForm.controls.lastname.value
+    newEmployee.email = this.registerForm.controls.email.value
+    newEmployee.username = this.registerForm.controls.firstname.value + this.registerForm.controls.lastname.value
+    let newPassword = Math.floor(Math.random() *999)
+    newEmployee.password = this.registerForm.controls.firstname.value + newPassword
+    newEmployee.isAdmin = false
+    console.log("Form ",newEmployee)
+
+    this.addEmployeeEmiter.emit(newEmployee)
+  
+  }
+  
   
 }
